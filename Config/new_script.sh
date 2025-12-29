b87e8ef6f2534e1ef882504f10594966aff01063
ИльяС <ilyasokolov1672@gmail.com>
2025-12-30 00:49:56 +0400

    Поправил правила IPTABLES

--- Config/new_script.sh
+++ Config/new_script.sh
@@ -104,7 +104,7 @@ do
    echo "Внесу значения в базу данных"
     for IP_and_DNS in $massiv_ip_addresov
     do
-        psql -h localhost -p 5432 -U postgres For_Cron -c "INSERT INTO infa (\"Наименование ДНС Службы\", \"Имя ДНС Службы\", \"IP адрес\") VALUES ('$dns_provider', '$dns_name_provider', '$IP_and_DNS');"
+        psql -h localhost -p 5432 -U postgres For_Cron -c "INSERT INTO infa (\"Наименование ДНС Службы\", \"Имя ДНС Службы\", \"IP адрес\") VALUES ('$dns_provider', '$dns_name_provider', '$IP_and_DNS');" #>/dev/null
     done
 
     massiv_ip_addresov=""
@@ -115,7 +115,70 @@ done
 }
 DNS_Provider_And_Plus_BD_And_Plus_Firewall
 
+
+pervichnaya_nastroyka_iptables() {
+if [[ $(whoami) == "root" ]]; then
+    #echo $(whoami)
+    iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+    iptables -A INPUT -i lo -j ACCEPT
+else
+    #echo $(whoami)
+    sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    sudo iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+    sudo iptables -A INPUT -i lo -j ACCEPT
+fi
+
+}
+pervichnaya_nastroyka_iptables
+
+
 massiv_dns_name=($(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select distinct \"Наименование ДНС Службы\" from infa;"))
+
+
+
+iptables1() {
+for ip_address in ${massiv_dns_name[@]}; do
+
+    #Рабочий код:
+    massiv_ip_addresov2=($(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select \"IP адрес\" from infa where \"Наименование ДНС Службы\" = '$ip_address';"))
+    echo ${massiv_ip_addresov2[@]}
+
+    for ip in ${massiv_ip_addresov2[@]}; do
+        #user=$(whoami)
+        #echo $user
+        if [[ $(whoami) == "root" ]]; then
+            #echo $(whoami)
+            iptables -A INPUT -s $ip -p icmp -j ACCEPT
+        else
+            #echo $(whoami)
+            sudo iptables -A INPUT -s $ip -p icmp -j ACCEPT
+        fi
+
+    done
+
+done
+
+#Рабочий код:
+if [[ $(whoami) == "root" ]]; then
+    iptables -A INPUT -p icmp -j DROP
+else
+    sudo iptables -A INPUT -p icmp -j DROP
+fi
+
+
+}
+iptables1
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -157,15 +220,8 @@ iptables65505
 
 
 
-
-iptables80() {
-#Рабочий код:
-#massiv_dns_name=($(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select distinct \"Наименование ДНС Службы\" from infa;"))
-#echo ${massiv_dns_name[@]}
+iptables65506() {
 for ip_address in ${massiv_dns_name[@]}; do
-
-    #Рабочий код:
-    #echo $(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select \"IP адрес\" from infa where \"Наименование ДНС Службы\" = '$ip_address';")
 
     #Рабочий код:
     massiv_ip_addresov2=($(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select \"IP адрес\" from infa where \"Наименование ДНС Службы\" = '$ip_address';"))
@@ -176,10 +232,10 @@ for ip_address in ${massiv_dns_name[@]}; do
         #echo $user
         if [[ $(whoami) == "root" ]]; then
             #echo $(whoami)
-            iptables -A INPUT -p tcp --dport 80 -s $ip -j ACCEPT
+            iptables -A INPUT -p tcp --dport 65506 -s $ip -j ACCEPT
         else
             #echo $(whoami)
-            sudo iptables -A INPUT -p tcp --dport 80 -s $ip -j ACCEPT
+            sudo iptables -A INPUT -p tcp --dport 65506 -s $ip -j ACCEPT
         fi
 
     done
@@ -188,12 +244,66 @@ done
 
 #Рабочий код:
 if [[ $(whoami) == "root" ]]; then
-    iptables -A INPUT -p tcp --dport 80 -j DROP
-    iptables -A INPUT -p udp --dport 80 -j DROP
+    iptables -A INPUT -p tcp --dport 65506 -j DROP
+    iptables -A INPUT -p udp --dport 65506 -j DROP
 else
-    sudo iptables -A INPUT -p tcp --dport 80 -j DROP
-    sudo iptables -A INPUT -p udp --dport 80 -j DROP
+    sudo iptables -A INPUT -p tcp --dport 65506 -j DROP
+    sudo iptables -A INPUT -p udp --dport 65506 -j DROP
 fi
+
+
+}
+iptables65506
+
+
+
+
+
+
+
+
+
+
+
+
+###Рабочий код:
+### <- означает то что рабочую функцию для 80 порта выключил, стоит убрать только ###
+###iptables80() {
+#Рабочий код:
+#massiv_dns_name=($(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select distinct \"Наименование ДНС Службы\" from infa;"))
+#echo ${massiv_dns_name[@]}
+###for ip_address in ${massiv_dns_name[@]}; do
+
+    #Рабочий код:
+    #echo $(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select \"IP адрес\" from infa where \"Наименование ДНС Службы\" = '$ip_address';")
+
+    #Рабочий код:
+###    massiv_ip_addresov2=($(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select \"IP адрес\" from infa where \"Наименование ДНС Службы\" = '$ip_address';"))
+###    echo ${massiv_ip_addresov2[@]}
+
+###     for ip in ${massiv_ip_addresov2[@]}; do
+        #user=$(whoami)
+        #echo $user
+###         if [[ $(whoami) == "root" ]]; then
+            #echo $(whoami)
+###             iptables -A INPUT -p tcp --dport 80 -s $ip -j ACCEPT
+###         else
+            #echo $(whoami)
+###             sudo iptables -A INPUT -p tcp --dport 80 -s $ip -j ACCEPT
+###         fi
+
+###     done
+
+### done
+
+#Рабочий код:
+### if [[ $(whoami) == "root" ]]; then
+###     iptables -A INPUT -p tcp --dport 80 -j DROP
+###     iptables -A INPUT -p udp --dport 80 -j DROP
+### else
+###     sudo iptables -A INPUT -p tcp --dport 80 -j DROP
+###     sudo iptables -A INPUT -p udp --dport 80 -j DROP
+### fi
 
 
 
@@ -218,57 +328,77 @@ fi
 #for ip in ${ips[@]}; do
 #    echo $ip
 #done
-}
-iptables80
+### }
+### iptables80
 
 
 
 
 
-iptables443() {
+
+#Рабочий код:
+### <- означает то что рабочую функцию для 80 порта выключил, стоит убрать только ###
+###iptables443() {
 #Рабочий код:
 #massiv_dns_name=($(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select distinct \"Наименование ДНС Службы\" from infa;"))
 #echo ${massiv_dns_name[@]}
-for ip_address in ${massiv_dns_name[@]}; do
+###for ip_address in ${massiv_dns_name[@]}; do
 
     #Рабочий код:
-    massiv_ip_addresov2=($(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select \"IP адрес\" from infa where \"Наименование ДНС Службы\" = '$ip_address';"))
-    echo ${massiv_ip_addresov2[@]}
+###    massiv_ip_addresov2=($(psql -h localhost -p 5432 -U postgres -d For_Cron -t -c "select \"IP адрес\" from infa where \"Наименование ДНС Службы\" = '$ip_address';"))
+###    echo ${massiv_ip_addresov2[@]}
 
-    for ip in ${massiv_ip_addresov2[@]}; do
+###    for ip in ${massiv_ip_addresov2[@]}; do
         #user=$(whoami)
         #echo $user
-        if [[ $(whoami) == "root" ]]; then
+###        if [[ $(whoami) == "root" ]]; then
             #echo $(whoami)
-            iptables -A INPUT -p tcp --dport 443 -s $ip -j ACCEPT
-        else
+###            iptables -A INPUT -p tcp --dport 443 -s $ip -j ACCEPT
+###        else
             #echo $(whoami)
-            sudo iptables -A INPUT -p tcp --dport 443 -s $ip -j ACCEPT
-        fi
+###            sudo iptables -A INPUT -p tcp --dport 443 -s $ip -j ACCEPT
+###        fi
 
-    done
+###    done
 
-done
+###done
 
 #Рабочий код:
-if [[ $(whoami) == "root" ]]; then
-    iptables -A INPUT -p tcp --dport 443 -j DROP
-    iptables -A INPUT -p udp --dport 443 -j DROP
-else
-    sudo iptables -A INPUT -p tcp --dport 443 -j DROP
-    sudo iptables -A INPUT -p udp --dport 443 -j DROP
-fi
+###if [[ $(whoami) == "root" ]]; then
+###    iptables -A INPUT -p tcp --dport 443 -j DROP
+###    iptables -A INPUT -p udp --dport 443 -j DROP
+###else
+###    sudo iptables -A INPUT -p tcp --dport 443 -j DROP
+###    sudo iptables -A INPUT -p udp --dport 443 -j DROP
+###fi
 
+
+###}
+###iptables443
+
+
+smena_policy_input_and_block_all() {
+if [[ $(whoami) == "root" ]]; then
+            #echo $(whoami)
+            iptables -P INPUT DROP
+            iptables -A INPUT -j DROP
+        else
+            #echo $(whoami)
+            sudo iptables -P INPUT DROP
+            sudo iptables -A INPUT -j DROP
+        fi
 
 }
-iptables443
+smena_policy_input_and_block_all
+
+
 
 
 sohranenie_pravil_iptables() {
 os_release=$(uname -a)
 #if [[ $os_release == *"Ubuntu"* || $os_release == *"Debian"* ]]; then
 
-if [[ $os_release == *"MANJARO"* ]]; then
+if [[ $os_release == *"MANJARO"* || $os_release == *"archlinux"* ]]; then
     sudo su - root -c "iptables-save > /etc/iptables/iptables.rules"
 else
     echo "ты юзаешь какую то другую систему чел"
